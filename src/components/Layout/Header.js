00860b779dfe6d581a027c4108c8c172a8fab970
ipcampbell12 <ipcampbell12@gmail.com>
2023-05-24 15:12:07 -0700

    header styling

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,14 +2,15 @@ import { Fragment } from 'react';
 
 import React from 'react';
 import bookshelfimage from '../../assets/bookshelfimage.jpg'
+import classes from './Header.module.css'
 
 function Header(props) {
     return (
         <Fragment>
-            <header>
+            <header className={classes.header}>
                 <h1>Book Log</h1>
             </header>
-            <div>
+            <div className={classes['main-image']}>
                 <img src={bookshelfimage} alt="A bookshelf" />
             </div>
         </Fragment>
