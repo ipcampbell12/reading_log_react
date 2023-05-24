609f1219253ad229d5aa5901525c1c7837b1d239
ipcampbell12 <ipcampbell12@gmail.com>
2023-05-24 15:22:53 -0700

    button styling

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,13 +3,17 @@ import { Fragment } from 'react';
 import React from 'react';
 import bookshelfimage from '../../assets/bookshelfimage.jpg'
 import classes from './Header.module.css'
+import LogButton from './LogButton';
+
 
 function Header(props) {
     return (
         <Fragment>
             <header className={classes.header}>
                 <h1>Book Log</h1>
+                <LogButton />
             </header>
+
             <div className={classes['main-image']}>
                 <img src={bookshelfimage} alt="A bookshelf" />
             </div>
